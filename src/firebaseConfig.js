353653135178b4c1775c401ba80625a039d2ce66
devs-sourav <souravacharjee361@gmail.com>
2023-10-03 17:59:24 +0600

    MUi and Google Font Satup

---
+++ src/firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD-qCi4Jkgb5RfqtxwH1rrjM1oxNQTVIf0",
+  authDomain: "linkedin-a90c2.firebaseapp.com",
+  projectId: "linkedin-a90c2",
+  storageBucket: "linkedin-a90c2.appspot.com",
+  messagingSenderId: "339001063858",
+  appId: "1:339001063858:web:0ee980c22d06ea9dc50cab"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default firebaseConfig
